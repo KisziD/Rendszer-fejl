ce654349c230b053942027a8ebf3487e2480ae91
KisziD <kiszidomi1@gmail.com>
2022-03-28 14:18:08 +0200

    Added SpecialityController

---
+++ Backend/Controllers/SpecialityController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Backend.Contexts;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialityController : ControllerBase
+    {
+
+        private readonly DatabaseContext context;
+
+        public SpecialityController(DatabaseContext cont)
+        {
+            context = cont;
+        }
+
+        [HttpGet]
+        public IEnumerable<Speciality> get()
+        {
+            return context.Specialities;
+        }
+
+        [HttpGet("names")]
+        public IEnumerable<string> names()
+        {
+            List<string> names = new List<string>();
+            foreach (var specialist in context.Specialities)
+            {
+                names.Add(specialist.Name);
+            }
+            return names;
+        }
+
+        [HttpGet("names/{id}")]
+        public string? name(int id)
+        {
+            return "{\"name\":\"" + context.Specialities.Where(s => s.ID == id).FirstOrDefault()?.Name + "\"}";
+        }
+
+        [HttpGet("all/{id}")]
+        public Speciality? getById(int id)
+        {
+            return context.Specialities.Where(s => s.ID == id).FirstOrDefault();
+        }
+
+        [HttpPost("add")]
+        public string post([FromBody] Speciality speciality)
+        {
+            context.Specialities.Add(speciality);
+            context.SaveChanges();
+            return "{\"response\":0}";
+        }
+
+        [HttpDelete("{id}")]
+        public string delete(int id)
+        {
+            context.Specialities.Remove(context.Specialities.Where(s => s.ID == id).First());
+            context.SaveChanges();
+            return "{\"response\":0}";
+        }
+
+    }
+
+}
---
+++ Backend/Models/Speciality.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    [Table("Speciality")]
+    public class Speciality
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
