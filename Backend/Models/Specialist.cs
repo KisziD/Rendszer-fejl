9fd83aa307d66bd2f4ba84feecdc6b585a7e38bb
KisziD <kiszidomi1@gmail.com>
2022-04-23 17:38:32 +0200

    Specialist and new user creation

--- Backend/Models/Specialist.cs
+++ Backend/Models/Specialist.cs
@@ -9,7 +9,16 @@ namespace Backend.Models
         [Key]
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Qualification { get; set; }
-        public string UserID { get; set; }
+        public int? Qualification { get; set; }
+        public int UserID { get; set; }
+    }
+
+    public class NewSpecialist
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string SpecialityName { get; set; }
     }
 }
